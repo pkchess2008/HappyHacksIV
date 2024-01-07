4a5f62b9101b6946d04d2c7d2fbe1feddd11e3a9
stephenmaina2995 <stephenmaina2995@gmail.com>
2024-01-07 17:26:39 +0300

    Configured the frontend and backend merge

---
+++ client/src/app/page.tsx
@@ -0,0 +1,22 @@
+// import Image from 'next/image'
+"use client";
+
+import React,{useState,useEffect} from 'react'
+
+export default function Home() {
+
+  const [message, setMessage]=useState("message")
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/api/home")
+    .then(res => res.json())
+    .then((data)=>{
+      // console.log(data);
+      setMessage(data.message);
+    });
+  },[]);
+
+  return (
+    <div>{message}</div>
+  )
+}
