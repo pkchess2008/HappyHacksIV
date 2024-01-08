cdd2f918649e26c1739b4efcf3d207bb385119c9
stephenmaina2995 <stephenmaina2995@gmail.com>
2024-01-08 05:26:25 +0300

    Added firebase authentication

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,34 @@
+import pyrebase
 from flask import Flask, jsonify,request
 from flask_cors import CORS
 from flask_socketio import SocketIO,send
 
 app = Flask(__name__)
-app.config['SECRET']="secret!123"
+# app.config['SECRET']="secret!123"
 socketio= SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({"message": "Migrants Social Media!"})
+
+config = {
+    apiKey: "AIzaSyBVEGPm_8MeW1iry85HX3lH-ZXUjvOCQG4",
+    authDomain: "migrant-social-media.firebaseapp.com",
+    projectId: "migrant-social-media",
+    storageBucket: "migrant-social-media.appspot.com",
+    messagingSenderId: "145470124849",
+    appId: "1:145470124849:web:e5d82a3cbdca738828f610",
+    measurementId: "G-QKR0QF8CNZ"
+  }
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+email=""
+# print user
+
+
 
 @socketio.on('message')
 def handle_message(message):
